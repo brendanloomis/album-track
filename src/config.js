40eaef4dcbe9af4d74bda27cede625b7adfc39ca
Brendan Loomis <brendanloomis@gmail.com>
2023-07-24 13:34:28 -0700

    update the API endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://album-track-api.herokuapp.com/api',
+    API_ENDPOINT: 'https://album-track-api.fly.dev/api',
     API_KEY: process.env.REACT_APP_API_KEY
 };
